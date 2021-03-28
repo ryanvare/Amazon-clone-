4ca56e0db166e23a99ebbe19c24142a08975db38
ryanvare <52427786+ryanvare@users.noreply.github.com>
2021-03-28 11:03:18 -0700

    react update

---
+++ amazon-clone-challenge/src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+function Header() {
+    return (
+        <div>
+           <HeaderLogo>
+            ryan
+           </HeaderLogo>
+        </div>
+    )
+}
+
+export default Header
+
+const HeaderLogo = styled.div`
+height: 60px;
+background-color: #0F1111;`
